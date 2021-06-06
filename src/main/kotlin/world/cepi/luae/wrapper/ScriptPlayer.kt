d6d2e1bbdd2f69b9f591d16bf43e758256913f1c
LeoDog896 <LeoDog896@hotmail.com>
2021-06-06 13:08:35 -0400

    Allow more player modification methods

--- src/main/kotlin/world/cepi/luae/wrapper/ScriptPlayer.kt
+++ src/main/kotlin/world/cepi/luae/wrapper/ScriptPlayer.kt
@@ -15,6 +15,26 @@ class ScriptPlayer(val player: Player) {
     }
 
     @HostAccess.Export
+    fun teleport(position: ScriptPosition) {
+        player.teleport(position.toPosition())
+    }
+
+    @get:HostAccess.Export
+    val position = player.position
+
+    @get:HostAccess.Export
+    val latency = player.latency
+
+    @get:HostAccess.Export
+    @set:HostAccess.Export
+    var flying: Boolean
+        get() = player.isFlying
+        set(value) {
+            player.isFlying = value
+        }
+
+
+    @get:HostAccess.Export
     val username: String = player.username
 
 }
