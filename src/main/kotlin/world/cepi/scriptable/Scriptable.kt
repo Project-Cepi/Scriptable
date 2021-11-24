8c9e446b1a29cd3ffddd1e2f197972f9c03ef4a9
LeoDog896 <LeoDog896@hotmail.com>
2021-11-24 13:07:19 -0500

    Make actions optional

--- src/main/kotlin/world/cepi/scriptable/Scriptable.kt
+++ src/main/kotlin/world/cepi/scriptable/Scriptable.kt
@@ -2,6 +2,7 @@ package world.cepi.scriptable
 
 import net.minestom.server.extensions.Extension;
 import world.cepi.actions.list.ActionManager
+import world.cepi.kstom.Manager
 import world.cepi.scriptable.command.ScriptCommand
 import world.cepi.scriptable.script.action.ScriptAction
 
@@ -9,16 +10,22 @@ class Scriptable : Extension() {
 
     override fun initialize() {
 
-        ActionManager.add(ScriptAction::class)
-
         ScriptCommand.register()
 
         logger.info("[Scriptable] has been enabled!")
     }
 
+    override fun postInitialize() {
+        if (Manager.extension.hasExtension("actions")) {
+            ActionManager.add(ScriptAction::class)
+        }
+    }
+
     override fun terminate() {
 
-        ActionManager.remove(ScriptAction::class)
+        if (Manager.extension.hasExtension("actions")) {
+            ActionManager.remove(ScriptAction::class)
+        }
 
         ScriptCommand.unregister()
 
